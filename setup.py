d44998737771c42c40370b9363c93f6e346c5e69
mattack108 <matt.lenc@gmail.com>
2013-09-03 13:31:29 +0100

    0.0.1 there you go

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pipin',
-    version='0.1.0',
+    version='0.0.1',
     description='',
     author='Matt Lenc',
     author_email='matt.lenc@gmail.com',
