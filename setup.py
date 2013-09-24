e258ca6c92491f5675b5a4e04ba5ec6c8ae01b61
mattack108 <matt.lenc@gmail.com>
2013-09-24 13:31:52 +0100

    Better app layout (for the last time)

--- setup.py
+++ setup.py
@@ -1,17 +1,39 @@
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+import pipin
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
 
 setup(
     name='pipin',
-    version='0.0.1',
+    version=pipin.__version__,
     description='',
     author='Matt Lenc',
     author_email='matt.lenc@gmail.com',
     url='http://github.com/mattack108/pipin',
     license='LICENSE.txt',
     packages=['pipin'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    test_suite='pipin.tests.test_pipin',
+    extras_require={
+        'testing': ['pytest'],
+    },
     entry_points={
         'console_scripts': [
-            'pipin = pipin:main',
+            'pipin = pipin.pipin:lets_pipin',
         ]
     },
     classifiers=[
