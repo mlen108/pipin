9122db19d57e1b154398385b74333c3f9dbc4378
mattack108 <matt.lenc@gmail.com>
2013-09-27 07:23:20 +0100

    Update classifiers in setup

--- setup.py
+++ setup.py
@@ -39,7 +39,11 @@ setup(
     classifiers=[
         "Environment :: Console",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
         "Topic :: Software Development",
         "Topic :: Utilities",
     ]
